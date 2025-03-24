6b31a29af17a863c9a2d4d0bd6920474335ef609
johaninho17 <johanyamssi@gmail.com>
2025-03-23 21:29:08 -0700

    index text added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,15 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'paris': "url('/src/images/paris.png')",
+				'london': "url('/src/images/london.png')",
+				'berlin': "url('/src/images/berlin.png')",
+				'stockholm': "url('/src/images/stockholm.png')",
+				'brussels': "url('/src/images/brussels.png')",
+				'amsterdam': "url('/src/images/amsterdam.png')",
+				'bluehue': "url('/src/images/blue-hue.png')",
+			},
 			fontFamily: {
 				'roboto': ['Roboto', 'sans-serif'],
 				'nunito': ["'Nunito'", 'sans-serif'],
