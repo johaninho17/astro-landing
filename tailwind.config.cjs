0db1dfbe8c85e030408f3ff7b6808fef650ea4ff
johaninho17 <johanyamssi@gmail.com>
2025-03-31 23:29:29 -0700

    about us and theme changes, moved images to public

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,14 +5,14 @@ module.exports = {
 	theme: {
 		extend: {
 			backgroundImage: {
-				'paris': "url('/src/images/paris.png')",
-				'london': "url('/src/images/london.png')",
-				'berlin': "url('/src/images/berlin.png')",
-				'stockholm': "url('/src/images/stockholm.png')",
-				'brussels': "url('/src/images/brussels.png')",
-				'amsterdam': "url('/src/images/amsterdam.png')",
-				'bluehue': "url('/src/images/blue-hue.png')",
-				'meeting': "url('/src/images/positive-startup-group-with-laptops-chatting-meeting-room.png')",
+				'paris': "url('/images/paris.png')",
+				'london': "url('/images/london.png')",
+				'berlin': "url('/images/berlin.png')",
+				'stockholm': "url('/images/stockholm.png')",
+				'brussels': "url('/images/brussels.png')",
+				'amsterdam': "url('/images/amsterdam.png')",
+				'bluehue': "url('/images/blue-hue.png')",
+				'meeting': "url('/images/positive-startup-group-with-laptops-chatting-meeting-room.png')",
 			},
 			fontFamily: {
 				'roboto': ['Roboto', 'sans-serif'],
